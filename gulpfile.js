9fd141b874c2d745c4d597c897d53384253d5290
mattia.astorino <astorino.design@gmail.com>
2016-01-03 18:23:36 +0100

    add test theme compiler

--- gulpfile.js
+++ gulpfile.js
@@ -9,79 +9,69 @@
 // Compiler settings
 // #############################################################################
 
-// Paths settings
-var distPath        = './www'; // Your "distribution" folder
-var cssPath         = '/css';   // Must be inside distPath
-var serverPath      = './www'; // Local server root folder
-
 // Compiler settings
-var minifyCSS       = true;
 var srcPath         = './src';
-var notifyLogo      = './pp.png';
-var defaultCSS      = false;
+var notifyLogo      = './icon.png';
 
-// Browser support for browser-sync
-var browser_support = [ 'last 2 versions' ];
 
 // Modules loader
-var gulp         = require( 'gulp'),
-    browserSync  = require( 'browser-sync'),
-    path         = require( 'path' ),
-    duration     = require('gulp-duration'),
-    notify       = require( 'gulp-notify' );
+var gulp        = require( 'gulp'),
+    path        = require( 'path' ),
+    duration    = require('gulp-duration'),
+    gutil       = require('gulp-util'),
+    clean       = require('gulp-clean'),
+    runSequence = require( 'run-sequence' ),
+    concat_json = require("gulp-concat-json"),
+    rename      = require('gulp-rename'),
+    concat      = require('gulp-concat-json2js'),
+    wrap        = require('gulp-wrap'),
+    notify      = require( 'gulp-notify' );
 
 
-// LESS Task Compiler
+// Theme builder
 // #############################################################################
 
-// Minify setting override
-if ( minifyCSS == true ) { var defaultCSS = false; }
-else if ( minifyCSS == false ) { var defaultCSS = true; }
 
-// Compiler
-gulp.task('themeBuilder', function() {
-    return gulp.src( srcPath + '/main.scss' )
 
-    .pipe( newer( distPath + cssPath ) )
 
-    // Init sassGlob plugin
-    .pipe( sassGlob() )
+gulp.task('themeBuilder', function () {
+  gulp.src('./src/**.json')
+ .pipe(concat())
+ .pipe(wrap('[\r\n <%= contents %> \r\n]'))
+ .pipe( rename({ extname: ".sublime-theme" }) )
+ .pipe(gulp.dest('./'))
+ .pipe( notify({
+    title: "Material Theme compiled",
+    message: "Material Theme compiled",
+    icon: path.join( __dirname, notifyLogo )
+  }))
+})
 
-    // Running sass compiler
-    .pipe( sass().on('error', sass.logError) )
 
-    // Running autoprefixer and write original file
-    .pipe( autoprefixer({ browsers: browser_support }) )
-    .pipe( gulpif( defaultCSS, gulp.dest( distPath + cssPath ) ) )
 
-    // Minifing and write min files
-    .pipe( gulpif( minifyCSS, rename( {suffix: '.min'} ) ) )
-    .pipe( gulpif( minifyCSS, minifycss() ) )
-    .pipe( duration('rebuilding files') )
-    .pipe( gulp.dest( distPath + cssPath ) )
-
-    // Show notification
-    .pipe( notify( {
-        title: "ProntoPro Design Test",
-        message: "CSS compilato con successo",
-        icon: path.join( __dirname, notifyLogo )
-    } ) )
+// cleaner
+gulp.task( 'build-clean', function () {
+    return gulp.src( './test.txt', {read: false} )
+    .pipe( clean( ) );
 });
-
 
 
 // Base watcher task
 // #############################################################################
 
 gulp.task( 'watch', function() {
-    gulp.watch( srcPath + '/**/*.scss', [ 'themeBuilder' ] );
+    gulp.watch( srcPath + '/**/*.json', [ 'builder' ] );
 });
 
 
 // Registered tasks
 // #############################################################################
 
-gulp.task( 'default', [ 'themeBuilder', 'watch' ] );
-gulp.task( 'build', function( callback ) {
-    runSequence( 'clean', 'builder', callback );
+/*gulp.task( 'default', [ 'clean', 'themeBuilder', 'watch' ] );*/
+gulp.task( 'default', function( callback ) {
+  runSequence( 'build-clean', 'themeBuilder', 'watch', callback );
+});
+
+gulp.task( 'builder', function( callback ) {
+  runSequence( 'build-clean', 'themeBuilder', callback );
 });
